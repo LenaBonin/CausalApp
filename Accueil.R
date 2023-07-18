9cedc6d41e41d6da5eaa1469600b65bff8c1d0d1
LenaBonin <lena.bonin@gmail.com>
2023-07-18 15:46:13 +0200

    Mise en forme Accueil

--- Accueil.R
+++ Accueil.R
@@ -5,11 +5,12 @@ fluidRow(
          Cette application a pour objectif de vous guider dans le choix des estimands et des methodes pour effectuer une annalyse causale,
          une analyse de mediation, ou une analyse d'interaction. Elle a ete developper dans un cadre applique à l'epidemiologie sociale<br>
          <br>
-         Pour commencer, rendez vous sur l'onglet questionnaire! <br>
+         <font size = +1> <strong>
+         Pour commencer, rendez vous sur l'onglet questionnaire!  </strong> </font> <br>
 <br>
 
-<font size = +1> <strong>
-More info about the app can be found on <a href = https://github.com/LenaBonin/CausalApp> https://github.com/LenaBonin/CausalApp </a> </strong> </font> <br>
+
+More info about the app can be found on <a href = https://github.com/LenaBonin/CausalApp> https://github.com/LenaBonin/CausalApp </a> <br>
 <br>")
   )
   
