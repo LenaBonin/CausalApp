27a07b3402ada258822f6e8065c15a9b515e3958
LenaBonin <lena.bonin@gmail.com>
2023-11-28 10:36:55 +0100

    Ajout logos

--- Accueil.R
+++ Accueil.R
@@ -13,7 +13,20 @@ fluidRow(
 
 
 Le code de l'application est disponible au lien suivant : <a href = https://github.com/LenaBonin/CausalApp> https://github.com/LenaBonin/CausalApp </a> <br>
-<br>")
+<br>
+           
+           <br>
+            <a href='https://www.inserm.fr/' target = '_blank'> <img src='logo_inserm.png' width='230' height='120' /></a>
+            <a href='https://cerpop.inserm.fr/' target = '_blank'><img src='ACERPOP.png' width='300' height='170' /></a>
+            <a href='http://gendhi.eu/' target = '_blank'><img src='logo_gendhi.jpg' width='300' height='200' /></a>
+            <a href='https://erc.europa.eu/homepage' target = '_blank'><img src='LOGO_ERC.jpg' width='340' height='220' /></a>")
   )
   
 )
+
+
+  
+#<a href='https://www.inserm.fr/'><img src='logo_inserm.png' width='170' height='100' /></a>
+#<a href='https://cerpop.inserm.fr/'><img src='ACERPOP.png' width='200' height='120' onclick='window.open(this.src, '_blank');'/></a>
+#<a href='http://gendhi.eu/'><img src='logo_gendhi.jpg' width='200' height='140' /></a>
+#<a href='https://erc.europa.eu/homepage'><img src='LOGO_ERC.jpg' width='210' height='130' /></a>
